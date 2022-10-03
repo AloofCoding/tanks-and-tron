c6f92abafa3a28dbc348768d135d2f6f34039c95
Andreas Steiner <andreas.steiner@hak-kitz.ac.at>
2022-10-03 15:26:36 +0200

    fixed aiming and driving

--- tanks-and-tron/TanksGame.xaml.cs
+++ tanks-and-tron/TanksGame.xaml.cs
@@ -25,36 +25,6 @@ namespace tanks_and_tron
             MainGrid.Focus();
         }
 
-        private void Window_MouseMove(object sender, MouseEventArgs e)
-        {
-            Point point = e.GetPosition(MainGrid);
-            var cannon_X = cannon.Margin.Left;
-            var cannon_Y = cannon.Margin.Top;
-            double side_a;
-            double side_b;
-
-            if(point.X < cannon_X)
-            {
-                side_b = cannon_X - point.X;
-            }
-            else
-            {
-                side_b = point.X - cannon_X;
-            }
-            if (point.Y < cannon_Y)
-            {
-                side_a = cannon_Y - point.Y;
-            }
-            else
-            {
-                side_a = point.Y - cannon_Y;
-            }
-
-            var side_c = sqrt(side_a ^ 2 + side_b ^ 2);
-        }
-
-
-
         private void MainGrid_KeyDown(object sender, KeyEventArgs e)
         {
             #region 2 keys
@@ -95,7 +65,56 @@ namespace tanks_and_tron
             */
             /*else*/
             #endregion
-            if (e.Key == Key.S)
+
+            if (e.Key == Key.W && Keyboard.IsKeyDown(Key.D) || e.Key == Key.D && Keyboard.IsKeyDown(Key.W))
+            {
+                Thickness mt = tank.Margin;
+                mt.Top -= 10;
+                mt.Left += 10;
+                tank.RenderTransform = new RotateTransform(45);
+                Thickness mc = cannon.Margin;
+                mc.Top -= 10;
+                mc.Left += 10;
+                tank.Margin = mt;
+                cannon.Margin = mc;
+            }
+            else if(e.Key == Key.S && Keyboard.IsKeyDown(Key.D) || e.Key == Key.D && Keyboard.IsKeyDown(Key.S))
+            {
+                Thickness mt = tank.Margin;
+                mt.Top += 10;
+                mt.Left += 10;
+                tank.RenderTransform = new RotateTransform(135);
+                Thickness mc = cannon.Margin;
+                mc.Top += 10;
+                mc.Left += 10;
+                tank.Margin = mt;
+                cannon.Margin = mc;
+            }
+            else if (e.Key == Key.S && Keyboard.IsKeyDown(Key.A) || e.Key == Key.A && Keyboard.IsKeyDown(Key.S))
+            {
+                Thickness mt = tank.Margin;
+                mt.Top += 10;
+                mt.Left -= 10;
+                tank.RenderTransform = new RotateTransform(225);
+                Thickness mc = cannon.Margin;
+                mc.Top += 10;
+                mc.Left -= 10;
+                tank.Margin = mt;
+                cannon.Margin = mc;
+            }
+            else if (e.Key == Key.W && Keyboard.IsKeyDown(Key.A) || e.Key == Key.A && Keyboard.IsKeyDown(Key.W))
+            {
+                Thickness mt = tank.Margin;
+                mt.Top -= 10;
+                mt.Left -= 10;
+                tank.RenderTransform = new RotateTransform(315);
+                Thickness mc = cannon.Margin;
+                mc.Top -= 10;
+                mc.Left -= 10;
+                tank.Margin = mt;
+                cannon.Margin = mc;
+            }
+            else if (e.Key == Key.S)
             {
                 Thickness mt = tank.Margin;
                 mt.Top += 10;
@@ -139,6 +158,54 @@ namespace tanks_and_tron
             {
 
             }
+        }
+
+        private void Window_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition(MainGrid);
+            var cannon_X = cannon.Margin.Left + cannon.Width/2;
+            var cannon_Y = cannon.Margin.Top + cannon.Height/2;
+            double side_a;
+            double side_b;
+
+            #region complicated dumb way for the 2 lines of code below
+            //if (point.X < cannon_X && point.Y < cannon_Y) // top right
+            //{
+            //    side_b = cannon_X - point.X;
+            //    side_a = cannon_Y - point.Y;
+            //}
+            //else if (point.X < cannon_X && point.Y > cannon_Y) // bottom right
+            //{
+            //    side_b = cannon_X - point.X;
+            //    side_a = cannon_Y - point.Y;
+            //}
+            //else if (point.X > cannon_X && point.Y < cannon_Y) // top left
+            //{
+            //    side_b = cannon_X - point.X;
+            //    side_a = cannon_Y - point.Y;
+            //}
+            //else if (point.X > cannon_X && point.Y > cannon_Y) // bottom left
+            //{
+            //    side_b = cannon_X - point.X;
+            //    side_a = cannon_Y - point.Y;
+            //}
+            //else // shouldn't happen !!!
+            //{
+            //    side_a = 0;
+            //    side_b = 0;
+            //}
+            #endregion
+
+            side_b = cannon_X - point.X;
+            side_a = cannon_Y - point.Y;
+
+            //var side_c = Math.Sqrt(Math.Pow(side_a, 2) + Math.Pow(side_b, 2));
+
+            float angle = (float)Math.Atan2(side_b,side_a) * (float)(180/Math.PI) * (-1);
+
+            //MessageBox.Show(angle.ToString());
+
+            cannon.RenderTransform = new RotateTransform(angle);
         }
     }
 }
