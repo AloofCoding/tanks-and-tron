27cbc403b3e92fd3c2b7007e36be2f9d4a441e6d
Christoph Steiner <christoph.steiner@hak-kitz.ac.at>
2022-10-03 15:56:44 +0200

    Sudoku: Column Check working

--- tanks-and-tron/SudokuGame.xaml.cs
+++ tanks-and-tron/SudokuGame.xaml.cs
@@ -22,52 +22,75 @@ namespace tanks_and_tron
         public SudokuGame()
         {
             InitializeComponent();
-            
         }
 
         private void btn_create_Click(object sender, RoutedEventArgs e)
         {
             btn_create.IsEnabled = false;
             txt_output.Text = "";
+            CreateSudoku();
+            MessageBox.Show("finished creation");
+            btn_create.IsEnabled = true;
+        }
+
+        private void CreateSudoku() 
+        {
             bool secondrow = false;
-            byte[] numbers = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[,] grid = new int[9,9];
+            int[,] grid = new int[9, 9];
             Random rnd = new Random();
-            
+            bool reshuffle = false;
+
             for (int iy = 0; iy < 9; iy++)
             {
                 int[] arr = Enumerable.Range(1, 9).OrderBy(c => rnd.Next()).ToArray();
                 for (int ix = 0; ix < 9; ix++)
                 {
-                    grid[iy, ix] = arr[ix]; //adds shuffled 1-9 to grid (horizontal)                   
+                    grid[iy, ix] = arr[ix]; //adds shuffled numbers 1-9 to grid (horizontal line)
                 }
 
-                //MessageBox.Show(grid.GetLength(0) + "");
-                
-                if (secondrow)
+                #region check of columns
+                if (secondrow) //activated after second row
                 {
-                    for (int ix = 0; ix < 8; ix++)
+                    for (int ix = 0; ix < 9; ix++) // ix = number of column
                     {
-                        HashSet<byte> set = new HashSet<byte>();
-                        for (int i = 0; i < iy; i++)
+                        HashSet<int> set = new HashSet<int>();
+                        for (int i = 0; i <= iy; i++)
                         {
-                            set.Add(((byte)grid[i, ix]));
+                            /*adding 
+                             * 0,0 | 
+                             * 1,0 | 
+                             * ... 
+                             * then checking whether the numbers are unique in that column*/
+                            set.Add(grid[i, ix]);
                         }
-                        if(set.Count < iy)
+                        if (set.Count <= iy)
                         {
-                            //MessageBox.Show("Reshuffling needed.");
+                            reshuffle = true;
+                            break;
                         }
                     }
                 }
-                for (int i = iy; i  <= iy; i++) 
-                {
-                    txt_output.Text += grid[i, 0] + " | " + grid[i, 1] + " | " + grid[i, 2] + " | " + grid[i, 3] + " | " + grid[i, 4] + " | " + grid[i, 5] + " | " + grid[i, 6] + " | " + grid[i, 7] + " | " + grid[i, 8] + Environment.NewLine;
-                } 
-                secondrow = true;
+                #endregion
 
+                #region Consequences of Checking
+                if (!reshuffle)
+                {
+                    for (int i = iy; i <= iy; i++)
+                    {
+                        txt_output.Text += grid[i, 0] + " | " + grid[i, 1] + " | " + grid[i, 2] + " | " + grid[i, 3] + " | " + grid[i, 4] + " | " + grid[i, 5] + " | " + grid[i, 6] + " | " + grid[i, 7] + " | " + grid[i, 8] + Environment.NewLine;
+                    }
+                }
+                if (reshuffle)
+                {
+                    iy -= 1;
+                    reshuffle = false;
+                }
+                #endregion
+
+                // ToDo: implement checking of 3x3 boxes and combine it with check of columns
+
+                secondrow = true;
             }
-            MessageBox.Show("finished with loop");
-            btn_create.IsEnabled = true;
         }
     }
 }
