c01021426a92dbf08276881db88cac800e36455a
Christoph Steiner <christoph.steiner@hak-kitz.ac.at>
2022-12-12 16:39:53 +0100

    generation complete plus partial infill

--- tanks-and-tron/SudokuGame.xaml.cs
+++ tanks-and-tron/SudokuGame.xaml.cs
@@ -30,7 +30,18 @@ namespace tanks_and_tron
             txt_output.Text = "";
             CreateSudoku();
             //ToDo: Delete selected numbers from the grid
+            int difficulty;
+            bool df = Int32.TryParse(cmb_difficulty.Text, out difficulty);
+            FillComplete();
 
+            if (df)
+            {
+                FillInSudoku(difficulty);
+            }
+            else
+            {
+                FillInSudoku(1);
+            }
             
         }
 
@@ -43,6 +54,8 @@ namespace tanks_and_tron
         int[,] block7 = new int[3, 3];
         int[,] block8 = new int[3, 3];
         int[,] block9 = new int[3, 3];
+        int[,] complete = new int[9, 9];
+        int[,] displayed = new int[9, 9];
 
         int s1;
         int s2;
@@ -74,15 +87,6 @@ namespace tanks_and_tron
                         i++;
                     }
                 }
-                txt_100.Text = block1[0, 0].ToString();
-                txt_101.Text = block1[0, 1].ToString();
-                txt_102.Text = block1[0, 2].ToString();
-                txt_110.Text = block1[1, 0].ToString();
-                txt_111.Text = block1[1, 1].ToString();
-                txt_112.Text = block1[1, 2].ToString();
-                txt_120.Text = block1[2, 0].ToString();
-                txt_121.Text = block1[2, 1].ToString();
-                txt_122.Text = block1[2, 2].ToString();
 
                 i = 0;
                 arr = Enumerable.Range(1, 9).OrderBy(c => rnd.Next()).ToArray();
@@ -94,15 +98,6 @@ namespace tanks_and_tron
                         i++;
                     }
                 }
-                txt_200.Text = block2[0, 0].ToString();
-                txt_201.Text = block2[0, 1].ToString();
-                txt_202.Text = block2[0, 2].ToString();
-                txt_210.Text = block2[1, 0].ToString();
-                txt_211.Text = block2[1, 1].ToString();
-                txt_212.Text = block2[1, 2].ToString();
-                txt_220.Text = block2[2, 0].ToString();
-                txt_221.Text = block2[2, 1].ToString();
-                txt_222.Text = block2[2, 2].ToString();
 
                 //checking if block 1 doesn't interfere with block 2 horizontally
 
@@ -187,15 +182,6 @@ namespace tanks_and_tron
             block3[2, 1] = numbersforblock3row3.ElementAt<int>(1);
             block3[2, 2] = numbersforblock3row3.ElementAt<int>(2);
 
-            txt_300.Text = block3[0, 0].ToString();
-            txt_301.Text = block3[0, 1].ToString();
-            txt_302.Text = block3[0, 2].ToString();
-            txt_310.Text = block3[1, 0].ToString();
-            txt_311.Text = block3[1, 1].ToString();
-            txt_312.Text = block3[1, 2].ToString();
-            txt_320.Text = block3[2, 0].ToString();
-            txt_321.Text = block3[2, 1].ToString();
-            txt_322.Text = block3[2, 2].ToString();
 
             //MessageBox.Show("block 3");
 
@@ -217,15 +203,6 @@ namespace tanks_and_tron
                         i++;
                     }
                 }
-                txt_400.Text = block4[0, 0].ToString();
-                txt_401.Text = block4[0, 1].ToString();
-                txt_402.Text = block4[0, 2].ToString();
-                txt_410.Text = block4[1, 0].ToString();
-                txt_411.Text = block4[1, 1].ToString();
-                txt_412.Text = block4[1, 2].ToString();
-                txt_420.Text = block4[2, 0].ToString();
-                txt_421.Text = block4[2, 1].ToString();
-                txt_422.Text = block4[2, 2].ToString();
 
                 //checking if block 4 doesn't interfere with block 1 vertically
 
@@ -323,15 +300,6 @@ namespace tanks_and_tron
             }
             ok = false;
 
-            txt_500.Text = block5[0, 0].ToString();
-            txt_501.Text = block5[0, 1].ToString();
-            txt_502.Text = block5[0, 2].ToString();
-            txt_510.Text = block5[1, 0].ToString();
-            txt_511.Text = block5[1, 1].ToString();
-            txt_512.Text = block5[1, 2].ToString();
-            txt_520.Text = block5[2, 0].ToString();
-            txt_521.Text = block5[2, 1].ToString();
-            txt_522.Text = block5[2, 2].ToString();
 
             //MessageBox.Show("block 5");
 
@@ -544,16 +512,6 @@ namespace tanks_and_tron
 
             } while (retry);
 
-            txt_600.Text = block6[0, 0].ToString();
-            txt_601.Text = block6[0, 1].ToString();
-            txt_602.Text = block6[0, 2].ToString();
-            txt_610.Text = block6[1, 0].ToString();
-            txt_611.Text = block6[1, 1].ToString();
-            txt_612.Text = block6[1, 2].ToString();
-            txt_620.Text = block6[2, 0].ToString();
-            txt_621.Text = block6[2, 1].ToString();
-            txt_622.Text = block6[2, 2].ToString();
-
             MessageBox.Show("block 6");
 
             #endregion
@@ -638,35 +596,6 @@ namespace tanks_and_tron
                 }
             } while (incorrect2);
             
-            txt_700.Text = block7[0, 0].ToString();
-            txt_710.Text = block7[1, 0].ToString();
-            txt_720.Text = block7[2, 0].ToString();
-            txt_701.Text = block7[0, 1].ToString();
-            txt_711.Text = block7[1, 1].ToString();
-            txt_721.Text = block7[2, 1].ToString();
-            txt_702.Text = block7[0, 2].ToString();
-            txt_712.Text = block7[1, 2].ToString();
-            txt_722.Text = block7[2, 2].ToString();
-
-            txt_800.Text = block8[0, 0].ToString();
-            txt_810.Text = block8[1, 0].ToString();
-            txt_820.Text = block8[2, 0].ToString();
-            txt_801.Text = block8[0, 1].ToString();
-            txt_811.Text = block8[1, 1].ToString();
-            txt_821.Text = block8[2, 1].ToString();
-            txt_802.Text = block8[0, 2].ToString();
-            txt_812.Text = block8[1, 2].ToString();
-            txt_822.Text = block8[2, 2].ToString();
-
-            txt_900.Text = block9[0, 0].ToString();
-            txt_910.Text = block9[1, 0].ToString();
-            txt_920.Text = block9[2, 0].ToString();
-            txt_901.Text = block9[0, 1].ToString();
-            txt_911.Text = block9[1, 1].ToString();
-            txt_921.Text = block9[2, 1].ToString();
-            txt_902.Text = block9[0, 2].ToString();
-            txt_912.Text = block9[1, 2].ToString();
-            txt_922.Text = block9[2, 2].ToString();
 
             #endregion
 
@@ -754,6 +683,194 @@ namespace tanks_and_tron
             block[2, 2] = numbers.ElementAt<int>(2);
 
             return block;
+        }
+
+        /// <summary>
+        /// Method for filling text in sudoku based on the selected difficulty. 1 = easy, 2 = medium, 3 = hard
+        /// </summary>
+        /// <param name="difficulty">1=easy, 2=medium, 3=hard, 666=diabolic</param>
+        private void FillInSudoku(int difficulty)
+        {
+            //ToDo: delete randomly selected values from Array int[,] displayed and print the remaining numbers.
+            Random rnd = new Random();
+            int fieldsleft;
+            switch (difficulty)
+            {
+                case 1:
+                    //Easy Sudoku puzzles come with at least three in each row, column, box, and number, along with 30 givens. https://www.sudokulovers.com/what-makes-Sudoku-easy-medium-or-hard
+                    fieldsleft = rnd.Next(35, 46);
+                    break;
+                case 2:
+                    fieldsleft = rnd.Next(27, 34);
+                    break;
+                case 3:
+                    fieldsleft = rnd.Next(25, 27);
+                    break;
+                case 666:
+                    fieldsleft = 20;
+                    break;
+                default:
+                    fieldsleft = rnd.Next(35, 45);
+                    break;
+            }
+
+            while (fieldsleft > 0)
+            {
+                //error because the array doesn't get shorter > infinte loop
+                int i = rnd.Next(0, 9);
+                int j = rnd.Next(0, 9);
+                if (displayed[i, j] != 0)
+                {
+                    displayed[i, j] = 0;
+                    fieldsleft--;
+                }
+            }
+
+            int[,] ablock1 = new int[3, 3];
+            int[,] ablock2 = new int[3, 3];
+            int[,] ablock3 = new int[3, 3];
+            int[,] ablock4 = new int[3, 3];
+            int[,] ablock5 = new int[3, 3];
+            int[,] ablock6 = new int[3, 3];
+            int[,] ablock7 = new int[3, 3];
+            int[,] ablock8 = new int[3, 3];
+            int[,] ablock9 = new int[3, 3];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    ablock1[i, j] = displayed[i, j];
+                    ablock2[i, j] = displayed[i, j + 3];
+                    ablock3[i, j] = displayed[i, j + 6];
+                    ablock4[i, j] = displayed[i + 3, j];
+                    ablock5[i, j] = displayed[i + 3, j + 3];
+                    ablock6[i, j] = displayed[i + 3, j + 6];
+                    ablock7[i, j] = displayed[i + 6, j];
+                    ablock8[i, j] = displayed[i + 6, j + 3];
+                    ablock9[i, j] = displayed[i + 6, j + 6];
+                }
+            }
+            
+
+            txt_100.Text = ablock1[0, 0].ToString();
+            txt_101.Text = ablock1[0, 1].ToString();
+            txt_102.Text = ablock1[0, 2].ToString();
+            txt_110.Text = ablock1[1, 0].ToString();
+            txt_111.Text = ablock1[1, 1].ToString();
+            txt_112.Text = ablock1[1, 2].ToString();
+            txt_120.Text = ablock1[2, 0].ToString();
+            txt_121.Text = ablock1[2, 1].ToString();
+            txt_122.Text = ablock1[2, 2].ToString();
+
+            txt_200.Text = ablock2[0, 0].ToString();
+            txt_201.Text = ablock2[0, 1].ToString();
+            txt_202.Text = ablock2[0, 2].ToString();
+            txt_210.Text = ablock2[1, 0].ToString();
+            txt_211.Text = ablock2[1, 1].ToString();
+            txt_212.Text = ablock2[1, 2].ToString();
+            txt_220.Text = ablock2[2, 0].ToString();
+            txt_221.Text = ablock2[2, 1].ToString();
+            txt_222.Text = ablock2[2, 2].ToString();
+
+            txt_300.Text = ablock3[0, 0].ToString();
+            txt_301.Text = ablock3[0, 1].ToString();
+            txt_302.Text = ablock3[0, 2].ToString();
+            txt_310.Text = ablock3[1, 0].ToString();
+            txt_311.Text = ablock3[1, 1].ToString();
+            txt_312.Text = ablock3[1, 2].ToString();
+            txt_320.Text = ablock3[2, 0].ToString();
+            txt_321.Text = ablock3[2, 1].ToString();
+            txt_322.Text = ablock3[2, 2].ToString();
+
+            txt_400.Text = ablock4[0, 0].ToString();
+            txt_401.Text = ablock4[0, 1].ToString();
+            txt_402.Text = ablock4[0, 2].ToString();
+            txt_410.Text = ablock4[1, 0].ToString();
+            txt_411.Text = ablock4[1, 1].ToString();
+            txt_412.Text = ablock4[1, 2].ToString();
+            txt_420.Text = ablock4[2, 0].ToString();
+            txt_421.Text = ablock4[2, 1].ToString();
+            txt_422.Text = ablock4[2, 2].ToString();
+
+            txt_500.Text = ablock5[0, 0].ToString();
+            txt_501.Text = ablock5[0, 1].ToString();
+            txt_502.Text = ablock5[0, 2].ToString();
+            txt_510.Text = ablock5[1, 0].ToString();
+            txt_511.Text = ablock5[1, 1].ToString();
+            txt_512.Text = ablock5[1, 2].ToString();
+            txt_520.Text = ablock5[2, 0].ToString();
+            txt_521.Text = ablock5[2, 1].ToString();
+            txt_522.Text = ablock5[2, 2].ToString();
+
+            txt_600.Text = ablock6[0, 0].ToString();
+            txt_601.Text = ablock6[0, 1].ToString();
+            txt_602.Text = ablock6[0, 2].ToString();
+            txt_610.Text = ablock6[1, 0].ToString();
+            txt_611.Text = ablock6[1, 1].ToString();
+            txt_612.Text = ablock6[1, 2].ToString();
+            txt_620.Text = ablock6[2, 0].ToString();
+            txt_621.Text = ablock6[2, 1].ToString();
+            txt_622.Text = ablock6[2, 2].ToString();
+
+            txt_700.Text = ablock7[0, 0].ToString();
+            txt_710.Text = ablock7[1, 0].ToString();
+            txt_720.Text = ablock7[2, 0].ToString();
+            txt_701.Text = ablock7[0, 1].ToString();
+            txt_711.Text = ablock7[1, 1].ToString();
+            txt_721.Text = ablock7[2, 1].ToString();
+            txt_702.Text = ablock7[0, 2].ToString();
+            txt_712.Text = ablock7[1, 2].ToString();
+            txt_722.Text = ablock7[2, 2].ToString();
+
+            txt_800.Text = ablock8[0, 0].ToString();
+            txt_810.Text = ablock8[1, 0].ToString();
+            txt_820.Text = ablock8[2, 0].ToString();
+            txt_801.Text = ablock8[0, 1].ToString();
+            txt_811.Text = ablock8[1, 1].ToString();
+            txt_821.Text = ablock8[2, 1].ToString();
+            txt_802.Text = ablock8[0, 2].ToString();
+            txt_812.Text = ablock8[1, 2].ToString();
+            txt_822.Text = ablock8[2, 2].ToString();
+
+            txt_900.Text = ablock9[0, 0].ToString();
+            txt_910.Text = ablock9[1, 0].ToString();
+            txt_920.Text = ablock9[2, 0].ToString();
+            txt_901.Text = ablock9[0, 1].ToString();
+            txt_911.Text = ablock9[1, 1].ToString();
+            txt_921.Text = ablock9[2, 1].ToString();
+            txt_902.Text = ablock9[0, 2].ToString();
+            txt_912.Text = ablock9[1, 2].ToString();
+            txt_922.Text = ablock9[2, 2].ToString();
+
+            //todo: ölkjfdsa
+            foreach (TextBox t in grid_sudoku)
+            {
+
+            }
+            MessageBox.Show("truly end reached");
+        }
+
+        private void FillComplete()
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                for(int j = 0; j <= 2; j++)
+                {
+                    complete[i, j] = block1[i, j];
+                    complete[i, j + 3] = block2[i, j];
+                    complete[i, j + 6] = block3[i, j];
+                    complete[i + 3, j] = block4[i, j];
+                    complete[i + 3, j + 3] = block5[i, j];
+                    complete[i + 3, j + 6] = block6[i, j];
+                    complete[i + 6, j] = block7[i, j];
+                    complete[i + 6, j + 3] = block8[i, j];
+                    complete[i + 6, j + 6] = block9[i, j];
+                }
+            }
+
+            displayed = complete;
+            //MessageBox.Show("some text");
         }
     }
 }
